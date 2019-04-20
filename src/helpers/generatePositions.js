294100876d8894c399b8cdb713fcfc6c2d27a7ff
jmpolitzer <jmpolitzer@gmail.com>
2019-04-20 09:56:27 -0500

    removes unnecessary conditional from generatePositions

--- src/helpers/generatePositions.js
+++ src/helpers/generatePositions.js
@@ -1,18 +1,16 @@
 export default function generatePositions(dimensions) {
   const blankArray = [...Array(dimensions)]
   const squares = blankArray.reduce((acc, _, i) => {
-    if (i < dimensions) {
-      blankArray.forEach((_, j) => {
-        const position = { x: i, y: j }
-        const square = {
-          position,
-          occupiedBy: j < 3 ? 1 : j > dimensions - 4 ? 2 : null,
-          isKinged: false
-        }
+    blankArray.forEach((_, j) => {
+      const position = { x: i, y: j }
+      const square = {
+        position,
+        occupiedBy: j < 3 ? 1 : j > dimensions - 4 ? 2 : null,
+        isKinged: false
+      }
 
-        acc.push(square)
-      })
-    }
+      acc.push(square)
+    })
 
     return acc
   }, [])
