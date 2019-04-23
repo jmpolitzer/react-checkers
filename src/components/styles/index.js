fdcc7cbada4a9db4679ac130abf99932f1442032
jmpolitzer <jmpolitzer@gmail.com>
2019-04-23 07:34:59 -0500

    begins adding mobile styles

--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -1,7 +1,11 @@
+const breakpoints = {
+  xSmall: 400
+}
+const mq = bp => `@media (max-width: ${breakpoints[bp]}px)`
+
 const checkerboard = {
   display: 'flex',
-  justifyContent: 'space-around',
-  width: '50%'
+  flexWrap: 'wrap'
 }
 
 const checkerboardRow = {
@@ -12,9 +16,13 @@ const boardSquare = isEvenPosition => ({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  height: 50,
-  width: 50,
-  backgroundColor: isEvenPosition ? 'black' : 'tan'
+  height: 45,
+  width: 45,
+  backgroundColor: isEvenPosition ? 'black' : 'tan',
+  [mq('xSmall')]: {
+    height: 45,
+    width: 45
+  }
 })
 
 const regularChecker = color => ({
@@ -22,7 +30,11 @@ const regularChecker = color => ({
   width: 35,
   borderRadius: '50%',
   backgroundColor: color,
-  cursor: 'pointer'
+  cursor: 'pointer',
+  [mq('xSmall')]: {
+    height: 30,
+    width: 30
+  }
 })
 
 const kingedChecker = color => ({
@@ -36,37 +48,57 @@ const kingedChecker = color => ({
   border: `5px solid ${color}`,
   color: color,
   fontWeight: 'bold',
-  fontSize: 17
+  fontSize: 17,
+  [mq('xSmall')]: {
+    height: 22,
+    width: 22,
+    fontSize: 12
+  }
 })
 
 const playerScoreboard = {
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center'
+  alignItems: 'center',
+  margin: '0px 20px',
+  [mq('xSmall')]: {
+    width: `100%`,
+    flexDirection: 'row',
+    margin: '20px 0px'
+  }
 }
 
 const playerName = (playerTurn, player, color) => ({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  height: '50px',
-  width: '50px',
+  height: 50,
+  width: 50,
   border: `5px solid ${color}`,
   borderRadius: '50%',
   color: playerTurn === player ? 'white' : color,
   backgroundColor: playerTurn === player ? color : 'white',
-  marginBottom: '10px'
+  marginBottom: '10px',
+  [mq('xSmall')]: {
+    height: 45,
+    width: 45,
+    marginBottom: '0px'
+  }
 })
 
 const playerScore = {
   display: 'flex',
-  width: '80px',
+  width: 80,
   flexWrap: 'wrap',
   justifyContent: 'space-around'
 }
 
 const capturedChecker = {
-  marginBottom: '5px'
+  marginBottom: '5px',
+  [mq('xSmall')]: {
+    height: 20,
+    width: 20
+  }
 }
 
 export {
